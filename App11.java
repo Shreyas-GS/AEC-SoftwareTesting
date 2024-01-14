d85a77649b413168eddce511537c026f7f4e5fda
AnkithaAKantli <132654652+AnkithaAKantli@users.noreply.github.com>
2024-01-15 00:11:51 +0530

    feat/homepagenavigation

--- App11.java
+++ App11.java
@@ -6,10 +6,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class App11 {
     public static void main(String[] args) throws InterruptedException{
-        System.setProperty("webdriver.chrome.driver", "/Users/shreyasgs/Documents/chromedriver-mac-arm64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ankitha A Kantli\\Desktop\\projectaec\\test\\src\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://store.steampowered.com/news/");
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//div[starts-with(@class, 'eventcalendar_FilterSettings')]//div[contains(., 'Options and Filters')]"))).click();
+        driver.get("https://store.steampowered.com/");
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//a[starts-with(@class, 'tab')]//span[contains(., 'News')]"))).click();
         Thread.sleep(4000);
         driver.quit();
     }
